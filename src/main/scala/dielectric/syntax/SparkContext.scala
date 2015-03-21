7a85e8e0c8fd3d5924a34e8d963aabbb0f461ba2
Adelbert Chang <adelbertc@gmail.com>
2015-03-20 20:05:43 -0700

    Accumulator starting w/ Monoid identity

--- src/main/scala/dielectric/syntax/SparkContext.scala
+++ src/main/scala/dielectric/syntax/SparkContext.scala
@@ -1,6 +1,6 @@
 package dielectric.syntax
 
-import org.apache.spark.{ AccumulatorParam, SparkContext }
+import org.apache.spark.{ Accumulator, AccumulatorParam, SparkContext }
 
 import spire.algebra.Monoid
 
@@ -9,15 +9,21 @@ trait SparkContextSyntax {
 }
 
 class SparkContextOps(val sc: SparkContext) extends AnyVal {
-  def monoidAccumulator[A : Monoid](start: A) = {
+  def monoidAccumulator[A : Monoid](start: A): Accumulator[A] = {
     val acc = new MonoidAccumulatorParam[A]
     sc.accumulator(start)(acc)
   }
 
-  def monoidAccumulatorWithName[A : Monoid](start: A, name: String) = {
+  def monoidAccumulatorWithName[A : Monoid](start: A, name: String): Accumulator[A] = {
     val acc = new MonoidAccumulatorParam[A]
     sc.accumulator(start, name)(acc)
   }
+
+  def zeroAccumulator[A](implicit A: Monoid[A]): Accumulator[A] =
+    monoidAccumulator(A.id)
+
+  def zeroAccumulatorWithName[A](name: String)(implicit A: Monoid[A]): Accumulator[A] =
+    monoidAccumulatorWithName(A.id, name)
 }
 
 private class MonoidAccumulatorParam[A](implicit A: Monoid[A]) extends AccumulatorParam[A] {
